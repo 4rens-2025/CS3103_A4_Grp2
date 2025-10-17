69d0acbb1560a8812766117816a26cf534c794da
Nereus Ng Wei Bin <iamnereusng@gmail.com>
2025-10-17 23:42:27 +0800

    Send packets with specific rate & duration

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from typing import Tuple
 
 from game_net_api.receiver import GameNetReceiver
 from game_net_api.sender import GameNetSender
-import random
 
 
 def print_received_packet(app_name: str, addr: Tuple[str, int], seq: int, ch: int, payload: bytes):
@@ -26,6 +25,31 @@ def print_metrics(sender_metric, receiver_metric):
     )
 
 
+async def _send_packets_worker(
+    sender: GameNetSender, dest: Tuple[str, int], rate: float, duration: float, reliable: bool
+):
+    interval = 1.0 / rate
+
+    loop = asyncio.get_running_loop()
+    t0 = loop.time()
+
+    next_send = t0
+    packet_idx = 0
+
+    while loop.time() < t0 + duration:
+        next_send += interval
+        sleep_for = next_send - loop.time()
+        if sleep_for > 0:
+            await asyncio.sleep(sleep_for)
+
+        await sender.send(
+            f"{"unreliable" if not reliable else "reliable"}-packet-{packet_idx}",
+            reliable=reliable,
+            dest=dest,
+        )
+        packet_idx += 1
+
+
 async def main():
     # Start the receiver
     receiver_addr = ("127.0.0.1", 50000)
@@ -43,29 +67,36 @@ async def main():
     sender = GameNetSender("Player 2", sender_addr)
     await sender.start()
 
-    reliable_curr = unreliable_curr = 0
-    for _ in range(100):
-        # reliable = random.choice([True, False])
-        reliable = True
-        await sender.send(
-            f"packet-{reliable_curr if reliable else unreliable_curr}",
-            reliable=reliable,
-            dest=receiver_addr,
+    # Start sending packets from sender to receiver
+    send_rate = 100.0  # packets per second
+    test_duration = 30.0  # seconds
+
+    tasks = [
+        asyncio.create_task(
+            _send_packets_worker(
+                sender,
+                dest=receiver_addr,
+                rate=send_rate,
+                duration=test_duration,
+                reliable=reliable,
+            )
         )
+        for reliable in [False, True]
+    ]
 
-        if reliable:
-            reliable_curr += 1
-        else:
-            unreliable_curr += 1
-
-    await asyncio.sleep(2)
+    await asyncio.gather(*tasks)
 
     # Clean up
-    sender_unreliable_metric, sender_reliable_metric = await sender.stop()
-    receiver_unreliable_metric, receiver_reliable_metric = await receiver.stop()
-    print("Sender and receiver stopped. Exiting.")
+    sender_reliable_metric, sender_unreliable_metric = await sender.stop()
+    receiver_reliable_metric, receiver_unreliable_metric = await receiver.stop()
+    print("Sender and receiver stopped. Exiting.\n")
 
+    print(f"Metrics Summary: (Packet rate: {send_rate} packets/sec over {test_duration} seconds)")
+    print("===============================================")
+    print("Unreliable Channel Metrics:")
     print_metrics(sender_unreliable_metric, receiver_unreliable_metric)
+    print("-----------------------------------------------")
+    print("Reliable Channel Metrics:")
     print_metrics(sender_reliable_metric, receiver_reliable_metric)
 
 
